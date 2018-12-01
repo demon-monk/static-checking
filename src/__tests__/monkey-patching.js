ad175190ccff0b18ea904ad65ce814511e76bcaf
Demon Monk <antdude@126.com>
2018-12-01 19:13:10 +0800

    add monkey patching to make getWinner function has a determinstic return so that we can test

--- src/__tests__/monkey-patching.js
+++ src/__tests__/monkey-patching.js
@@ -1,6 +1,11 @@
 const thumbWar = require('../thumb-war')
-// const utils = require('../utils')
+const utils = require('../utils')
+
+const originGetWinner = utils.getWinner
+utils.getWinner = p1 => p1
+
 test('bang should win', () => {
   const winner = thumbWar('Bang', 'Dan')
   expect(winner).toEqual('Bang')
+  utils.getWinner = originGetWinner
 })
