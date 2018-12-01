013be983aea668b69884fd5179c37ea08caf7be6
Demon Monk <antdude@126.com>
2018-12-01 22:06:16 +0800

    implement jest.spyOn and use it to restore mock

--- src/__testUtils/utils.js
+++ src/__testUtils/utils.js
@@ -1,10 +1,17 @@
-function fn(impl) {
+function fn(impl = () => {}) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
     return impl(...args)
   }
   mockFn.mock = { calls: [] }
+  mockFn.mockImplementation = newImpl => (impl = newImpl)
   return mockFn
 }
 
-module.exports = { fn }
+function spyOn(obj, prop, impl = () => {}) {
+  const originVal = obj[prop]
+  obj[prop] = fn(impl)
+  obj[prop].mockRestore = () => (obj[prop] = originVal)
+}
+
+module.exports = { fn, spyOn }
--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -1,9 +1,18 @@
 const utils = require('../utils')
 const thumbWar = require('../thumb-war')
+const { spyOn } = require('../__testUtils/utils')
 
 test('return winner', () => {
   jest.spyOn(utils, 'getWinner')
   utils.getWinner.mockImplementation(p1 => p1)
+  const winner = thumbWar('Bang', 'Dan')
+  expect(winner).toEqual('Bang')
+  utils.getWinner.mockRestore()
+})
+
+test('return winner', () => {
+  spyOn(utils, 'getWinner', p1 => p1)
+  //   utils.getWinner.mockImplementation(p1 => p1)
   const winner = thumbWar('Bang', 'Dan')
   expect(winner).toEqual('Bang')
   utils.getWinner.mockRestore()
